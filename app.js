821eacd8e373be4ff6e51337a01fd7066a8de07a
ajatwani <ajatwani.19@gmail.com>
2022-05-23 14:39:38 +0400

    navbar update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const menu = document.querySelector("#mobile-menu")
-const menuLinks = document.querySelector(".navbar_menu")
+const menu = document.querySelector(".menu")
+const menuLinks = document.querySelector(".navbar")
 
 menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
